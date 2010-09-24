1df4dd44f95e6915b3cf0d9a292e0de8fe3396a8
kevingabbert@gmail.com <kevingabbert@gmail.com@88909c0e-446c-b3ae-36aa-448b497ac207>
2010-09-24 04:43:03 +0000

    Starting to refactor.
    Testing out how it logs to a specific calendar.

---
+++ source/CalendarLogManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProcessCalendar
+{
+    public class CalendarLogManager: List<ProcessToRecord>
+    {
+        public void Add(ProcessToRecord process, bool stillRunning)
+        {
+            base.Add(process);
+        }
+
+        public void AddOrUpdate(ProcessToRecord process, bool stillRunning)
+        {
+            //look up by ID. (Use Linq)
+            ProcessToRecord item = this.Single(s => s == process);
+
+            //-can't find? create a new process
+
+            //If null then keep going
+
+            //- CAN find? Update the one in there
+            //- this[x] = process as ProcessToRecord;
+            //- this[x].StillRunning = stillRunning
+            item = process; //overwrite what was there
+            item.StillRunning = stillRunning;
+
+
+            //base.Add(process);
+        }
+
+        public int StilRunning { get; set; }
+    }
+}
---
+++ source/ProcessToRecord.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProcessCalendar
+{
+    public class ProcessToRecord: System.Diagnostics.Process
+    {
+        public bool StillRunning { get; set; }
+    }
+}
